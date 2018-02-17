a60d78c235c0415c60aa99992183e5bc66bb77c3
bmintz <bmintz@users.noreply.github.com>
2018-02-17 13:52:07 -0600

    back to big smoke sim

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-var getById = document.getElementById.bind(document)
+const foods = [
+	'two number 9s',
+	'a number 9 large',
+	'a number 6 with extra dip',
+	'a number 7',
+	'two number 45s, one with cheese',
+	'a large soda.']
+// in the current order, getting and removing elements in order would require foods.shift()
+// which is slow. reversing the array lets us pop() instead, which is O(1)
+// of course, this is all a little overkill for a silly meme anyway.
+foods.reverse()
 
-const numCookies = getById('numCookies')
+const order = document.querySelector('ol')
 
-getById('cookie').onclick = function() {
-	numCookies.innerHTML = parseInt(numCookies.innerHTML) + 1}
+document.querySelector('button').onclick = function() {
+	if (foods.length == 0)
+		return;
+	el = document.createElement('li')
+	el.innerHTML = foods.pop()
+	order.appendChild(el)}
