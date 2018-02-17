6a11270a2f908fbb5005568390cddfd798e0c22f
bmintz <bmintz@users.noreply.github.com>
2018-02-17 13:52:07 -0600

    big smoke simulator

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+const foods = [
+	'two number 9s',
+	'a number 9 large',
+	'a number 6 with extra dip',
+	'a number 7',
+	'two number 45s, one with cheese',
+	'a large soda.']
+
 document.querySelector('button').onclick = function() {
+	if (foods.length == 0)
+		return;
 	el = document.createElement('li')
-	el.innerHTML = 'no meat, no cheese, no V E G E T A L S'
+	el.innerHTML = foods.shift()
 	document.getElementById('exampleList').appendChild(el)}
